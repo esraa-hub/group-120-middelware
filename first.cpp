39e2541e73679d3ad86a8b71a5f6a709dc5f803a
esraa <digfidn@gmail.com>
2025-02-11 13:51:28 +0200

    remove brackets

--- first.cpp
+++ first.cpp
@@ -3,15 +3,11 @@
  int main()
  {
 
- int k;
-<<<<<<< HEAD
-  cin >>k;
-  cout << k;
 
-=======
  cin >> k;
   cout <<k;
   cout << "hello";
->>>>>>> d81d694ad41e4f28dd5818f6895d1233bdd4e9c4
+
+
 
  }
