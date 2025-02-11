be0b6ac0f4e76a1d8ba842487592b99bd776ef60
esraa <digfidn@gmail.com>
2025-02-11 13:13:38 +0200

    correct error

--- first.cpp
+++ first.cpp
@@ -1,13 +1,11 @@
  #include <iostream>
+  using namespace std;
  int main()
  {
-<<<<<<< HEAD
 
- std::cout<< "hello from branch update";
-=======
  int k;
- cin >> k;
-  cout >> k;
->>>>>>> update
+  cin >>k;
+  cout << k;
+
 
  }
